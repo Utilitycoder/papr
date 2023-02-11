363a4d1ea4f82018670c922a5b0dd403f8cc9aa8
Lawalrowship <lawal@rowship.com>
2023-02-11 20:42:53 +0100

    refaxtored addCollateral.t.sol

--- test/paprController/AddCollateral.t.sol
+++ test/paprController/AddCollateral.t.sol
@@ -17,7 +17,7 @@ contract AddCollateralTest is BasePaprControllerTest {
         _addCollateral();
         assertEq(controller.collateralOwner(collateral.addr, collateral.id), borrower);
     }
-
+    /// @notice Test adding collateral emits the necessary event
     function testAddCollateralEmitsAddCollateral() public {
         vm.startPrank(borrower);
         nft.approve(address(controller), collateralId);
@@ -28,22 +28,25 @@ contract AddCollateralTest is BasePaprControllerTest {
         controller.addCollateral(c);
     }
 
+    /// @notice test that adding collateral increases count in vault
     function testAddCollateralIncreasesCountInVault() public {
         uint256 beforeCount = controller.vaultInfo(borrower, collateral.addr).count;
         _addCollateral();
         assertEq(beforeCount + 1, controller.vaultInfo(borrower, collateral.addr).count);
     }
 
+    /// @notice Test that adding collateral fails if invalid NFT is added
     function testAddCollateralFailsIfInvalidCollateral() public {
         TestERC721 invalidNFT = new TestERC721();
         vm.startPrank(borrower);
         nft.approve(address(controller), collateralId);
         vm.expectRevert(IPaprController.InvalidCollateral.selector);
         IPaprController.Collateral[] memory c = new IPaprController.Collateral[](1);
-        c[0] = IPaprController.Collateral(ERC721(address(1)), 1);
+        c[0] = IPaprController.Collateral(ERC721(address(invalidNFT)), 1);
         controller.addCollateral(c);
     }
 
+    /// @notice Add NFT as collateral
     function _addCollateral() internal {
         vm.startPrank(borrower);
         nft.approve(address(controller), collateralId);
